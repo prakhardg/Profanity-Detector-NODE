d29eb8dbab07b9ec41d83c60cb96a010b8e929ea
Prakhar Dev Gupta <prakhar4msoffice@gmail.com>
2018-06-04 17:46:51 +0530

    Variable names meaningfully changed

--- stemmer.js
+++ stemmer.js
@@ -39,17 +39,17 @@ let stem = (function stem() {
             "ness": ""
         },
 
-        c = "[^aeiou]",          // consonant
-        v = "[aeiouy]",          // vowel
-        C = c + "[^aeiouy]*",    // consonant sequence
-        V = v + "[aeiou]*",      // vowel sequence
+        consonant = "[^aeiou]",          // consonant
+        vowel = "[aeiouy]",          // vowel
+        consonant_seq = consonant + "[^aeiouy]*",    // consonant sequence
+        vowel_seq = vowel + "[aeiou]*",      // vowel sequence
 
-        mgr0 = "^(" + C + ")?" + V + C,               // [C]VC... is m>0
-        meq1 = "^(" + C + ")?" + V + C + "(" + V + ")?$",  // [C]VC[V] is m=1
-        mgr1 = "^(" + C + ")?" + V + C + V + C,       // [C]VCVC... is m>1
-        s_v = "^(" + C + ")?" + v;                   // vowel in stem
+        mgr0 = "^(" + consonant_seq + ")?" + vowel_seq + consonant_seq,               // [consonant_seq]VC... is m>0
+        meq1 = "^(" + consonant_seq + ")?" + vowel_seq + consonant_seq + "(" + vowel_seq + ")?$",  // [consonant_seq]VC[vowel_seq] is m=1
+        mgr1 = "^(" + consonant_seq + ")?" + vowel_seq + consonant_seq + vowel_seq + consonant_seq,       // [consonant_seq]VCVC... is m>1
+        s_v = "^(" + consonant_seq + ")?" + vowel;                   // vowel in stem
 
-    return function (w) {
+    return function (word) {
         let stem,
             suffix,
             firstch,
@@ -57,126 +57,126 @@ let stem = (function stem() {
             re2,
             re3,
             re4,
-            origword = w;
+            origword = word;
 
-        if (w.length < 3) { return w; }
+        if (word.length < 3) { return word; }
 
-        firstch = w.substr(0, 1);
+        firstch = word.substr(0, 1);
         if (firstch == "y") {
-            w = firstch.toUpperCase() + w.substr(1);
+            word = firstch.toUpperCase() + word.substr(1);
         }
 
         // Step 1a
         re = /^(.+?)(ss|i)es$/;
         re2 = /^(.+?)([^s])s$/;
 
-        if (re.test(w)) { w = w.replace(re, "$1$2"); }
-        else if (re2.test(w)) { w = w.replace(re2, "$1$2"); }
+        if (re.test(word)) { word = word.replace(re, "$1$2"); }
+        else if (re2.test(word)) { word = word.replace(re2, "$1$2"); }
 
         // Step 1b
         re = /^(.+?)eed$/;
         re2 = /^(.+?)(ed|ing)$/;
-        if (re.test(w)) {
-            let fp = re.exec(w);
+        if (re.test(word)) {
+            let found_patterns = re.exec(word);
             re = new RegExp(mgr0);
-            if (re.test(fp[1])) {
+            if (re.test(found_patterns[1])) {
                 re = /.$/;
-                w = w.replace(re, "");
+                word = word.replace(re, "");
             }
-        } else if (re2.test(w)) {
-            let fp = re2.exec(w);
-            stem = fp[1];
+        } else if (re2.test(word)) {
+            let found_patterns = re2.exec(word);
+            stem = found_patterns[1];
             re2 = new RegExp(s_v);
             if (re2.test(stem)) {
-                w = stem;
+                word = stem;
                 re2 = /(at|bl|iz)$/;
                 re3 = new RegExp("([^aeiouylsz])\\1$");
-                re4 = new RegExp("^" + C + v + "[^aeiouwxy]$");
-                if (re2.test(w)) { w = w + "e"; }
-                else if (re3.test(w)) { re = /.$/; w = w.replace(re, ""); }
-                else if (re4.test(w)) { w = w + "e"; }
+                re4 = new RegExp("^" + consonant_seq + vowel + "[^aeiouwxy]$");
+                if (re2.test(word)) { word = word + "e"; }
+                else if (re3.test(word)) { re = /.$/; word = word.replace(re, ""); }
+                else if (re4.test(word)) { word = word + "e"; }
             }
         }
 
         // Step 1c
         re = /^(.+?)y$/;
-        if (re.test(w)) {
-            let fp = re.exec(w);
-            stem = fp[1];
+        if (re.test(word)) {
+            let found_patterns = re.exec(word);
+            stem = found_patterns[1];
             re = new RegExp(s_v);
-            if (re.test(stem)) { w = stem + "i"; }
+            if (re.test(stem)) { word = stem + "i"; }
         }
 
         // Step 2
         re = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/;
-        if (re.test(w)) {
-            let fp = re.exec(w);
-            stem = fp[1];
-            suffix = fp[2];
+        if (re.test(word)) {
+            let found_patterns = re.exec(word);
+            stem = found_patterns[1];
+            suffix = found_patterns[2];
             re = new RegExp(mgr0);
             if (re.test(stem)) {
-                w = stem + step2list[suffix];
+                word = stem + step2list[suffix];
             }
         }
 
         // Step 3
         re = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
-        if (re.test(w)) {
-            let fp = re.exec(w);
-            stem = fp[1];
-            suffix = fp[2];
+        if (re.test(word)) {
+            let found_patterns = re.exec(word);
+            stem = found_patterns[1];
+            suffix = found_patterns[2];
             re = new RegExp(mgr0);
             if (re.test(stem)) {
-                w = stem + step3list[suffix];
+                word = stem + step3list[suffix];
             }
         }
 
         // Step 4
         re = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/;
         re2 = /^(.+?)(s|t)(ion)$/;
-        if (re.test(w)) {
-            let fp = re.exec(w);
-            stem = fp[1];
+        if (re.test(word)) {
+            let found_patterns = re.exec(word);
+            stem = found_patterns[1];
             re = new RegExp(mgr1);
             if (re.test(stem)) {
-                w = stem;
+                word = stem;
             }
-        } else if (re2.test(w)) {
-            let fp = re2.exec(w);
-            stem = fp[1] + fp[2];
+        } else if (re2.test(word)) {
+            let found_patterns = re2.exec(word);
+            stem = found_patterns[1] + found_patterns[2];
             re2 = new RegExp(mgr1);
             if (re2.test(stem)) {
-                w = stem;
+                word = stem;
             }
         }
 
         // Step 5
         re = /^(.+?)e$/;
-        if (re.test(w)) {
-            let fp = re.exec(w);
-            stem = fp[1];
+        if (re.test(word)) {
+            let found_patterns = re.exec(word);
+            stem = found_patterns[1];
             re = new RegExp(mgr1);
             re2 = new RegExp(meq1);
-            re3 = new RegExp("^" + C + v + "[^aeiouwxy]$");
+            re3 = new RegExp("^" + consonant_seq + vowel + "[^aeiouwxy]$");
             if (re.test(stem) || (re2.test(stem) && !(re3.test(stem)))) {
-                w = stem;
+                word = stem;
             }
         }
 
         re = /ll$/;
         re2 = new RegExp(mgr1);
-        if (re.test(w) && re2.test(w)) {
+        if (re.test(word) && re2.test(word)) {
             re = /.$/;
-            w = w.replace(re, "");
+            word = word.replace(re, "");
         }
 
         // and turn initial Y back to y
 
         if (firstch == "y") {
-            w = firstch.toLowerCase() + w.substr(1);
+            word = firstch.toLowerCase() + word.substr(1);
         }
 
-        return w;
+        return word;
     }
 })();
 
